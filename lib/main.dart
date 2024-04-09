a4042fc682518429e561fc15551d7a0e3705b23a
Md.Sakibullah <sakibullah98@gmail.com>
2024-04-09 23:38:28 +0600

    updating main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:fitness/pages/home.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(), // Set HomePage as the home screen
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: 'Poppins'),
+        home: HomePage());
   }
 }
